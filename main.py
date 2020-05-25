586f02838704b68d3a7c1d42e247ddc68c2661a4
Zailux <50146015+Zailux@users.noreply.github.com>
2020-05-25 13:15:37 +0200

    requirements.txt + small main.py logging

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #Einfacherhalber ist aktuell es im Controller PY der Befehl
-from src.uvis_controller import UltraVisController
+import logging
+import time
+
 from src.config import Configuration
+from src.uvis_controller import UltraVisController
+
 _cfg = Configuration()
 
 if __name__ == "__main__":
+    logging.info("Startup TTRP")
+    start = time.process_time()
     controller = UltraVisController(debug_mode=_cfg.DEBUG)
+    end = time.process_time()
+    logging.info(f"TTRP started in {end-start} ms")
     controller.run()
