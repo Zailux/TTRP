4c4b9425525834c668f3ea47446fb17b57774fc6
Zailux <50146015+Zailux@users.noreply.github.com>
2020-08-19 21:59:19 +0200

    sphinx docs base files

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,12 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(1, '..\\') # For importing the src package
 sys.path.insert(2, os.path.abspath('src')) # For importing the src package
-
+sys.path.insert(3, 'src')
+sys.path.insert(4, os.path.abspath('./src'))
 
 # -- Project information -----------------------------------------------------
 
